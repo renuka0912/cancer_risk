1eeeaf6e1a3e24eed43de1ab1a42b40ba8f586f8
renuka0912 <23215a6708@bvrit.ac.in>
2025-07-08 12:58:54 +0530

    second-commit

--- train_model.py
+++ train_model.py
@@ -14,7 +14,7 @@ df['risk_colorectal_encoded'] =le_colorectal.fit_transform(df['risk_colorectal']
 df['has_family_history']=df['family_history'].apply(lambda x: 0 if x.strip().lower() == 'none' else 1)
 df['is_smoker']=df['lifestyle'].apply(lambda x:1 if'smoker'in x.lower()else 0)
 df['is_obese']=df['lifestyle'].apply(lambda x:1 if'obese'in x.lower() else 0)
-df['uses_alcohol'] df['lifestyle'].apply(lambda x:1 if'alcohol'in x.lower()else 0)
+df['uses_alcohol']=df['lifestyle'].apply(lambda x:1 if'alcohol'in x.lower()else 0)
 features = ['age','gender','has_family_history','is_smoker','is_obese','uses_alcohol']
 model_breast=RandomForestClassifier(random_state=42).fit(df[features], df['risk_breast_encoded'])
 model_cervical =RandomForestClassifier(random_state=42).fit(df[features], df['risk_cervical_encoded'])
